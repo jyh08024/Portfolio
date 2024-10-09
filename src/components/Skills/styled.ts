62a1bd4aeee2b7c05ddb6ecb646125c7b0c75373
장영훈 <jyh08024@gmail.com>
2024-10-10 02:15:48 +0900

    Skills Complete

--- src/components/Skills/styled.ts
+++ src/components/Skills/styled.ts
@@ -13,21 +13,20 @@ export const SkillsStyle = styled.div`
 export const SkillsList = styled.div`
 	width: 100%;
 
+	div {
+		margin: .4rem 0;
+		height: 30px;
+	}
+
 	p {
 		color: #f4f5fc;
-		margin: .4rem 0;
 		font-size: 1.6rem;
-		padding-left: 48px;
 		display: flex;
 		align-items: center;
 
 		&.yellow {
 			color: #febf00;
 			padding-left: 0;
-		}
-
-		&.under {
-			padding-left: 24px;
 		}
 
 		span {
@@ -41,8 +40,42 @@ export const SkillsList = styled.div`
 			margin-right: 12px;
 		}
 
-		&.inline {
-			padding-left: 64px;
+		&.explain_text {
+			font-size: 1.2rem;
+			opacity: .9;
+		}
+
+		&.dataKey {
+			color: #febf00;
+		}
+	}
+
+	.skill_item {
+		display: flex;
+		align-items: center;
+		column-gap: 8px;
+		height: auto;
+		margin: 0;
+
+		> div {
+			display: flex;
+			align-items: center;
+			padding: 8px 24px;
+			font-weight: bold;
+			height: auto;
+			border-radius: 99999px;
+			cursor: pointer;
+
+			&.main {
+				background: #febf00;
+				color: #333;
+			}
+
+			&.not_main {
+				background: #333;
+				border: 2px solid #febf00;
+				color: #f4f5fc;
+			}
 		}
 	}
 `;
