722a856d47439ed996c018a976cff2a5105573b4
장영훈 <80209790+jyh08024@users.noreply.github.com>
2024-10-11 20:32:59 +0900

    Proejct in progress

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,4 +1,10 @@
-import { ProjectItem, ProjectList, ProjectStyle } from "./styled";
+import {
+  ProjectImage,
+  ProjectInfo,
+  ProjectItem,
+  ProjectList,
+  ProjectStyle,
+} from "./styled";
 
 const Project = () => {
   return (
@@ -9,7 +15,34 @@ const Project = () => {
 
       <ProjectList>
         <ProjectItem>
-          <div className="project_title">PlanWith</div>
+          <ProjectImage>
+            <img
+              src="/assets/image/project/planwith/home_normal.png"
+              alt="project_image"
+            />
+          </ProjectImage>
+          <ProjectInfo>
+            <p>2024.06 ~ 2024.08</p>
+            <h2 className="project_title">플랜위드 (PlanWith)</h2>
+            <p className="project_type">팀 프로젝트 (2인)</p>
+
+            <div className="project_explain">
+              <p>귀여운 마스코트 캐릭터와 함께 하는 일정 관리 할 일 기록 앱</p>
+            </div>
+
+            <ul className="project_role">
+              <li>백엔드 API 개발, 프론트엔드 개발을 담당하였습니다.</li>
+              <li>백엔드 업무: 할 일 CURD API, 백로그 API 개발</li>
+              <li>
+                프론트 업무: 메인화면, 할 일 상세 페이지, 알람 화면, 튜토리얼
+                화면 담당
+              </li>
+            </ul>
+
+            <div className="project_skill">
+              <div>Dart-Flutter</div>
+            </div>
+          </ProjectInfo>
         </ProjectItem>
       </ProjectList>
     </ProjectStyle>
--- src/components/Project/styled.ts
+++ src/components/Project/styled.ts
@@ -8,12 +8,65 @@ export const ProjectStyle = styled.div`
 export const ProjectList = styled.div`
   width: 100%;
   height: 100%;
+  display: flex;
+  justify-content: center;
 `;
 
 export const ProjectItem = styled.div`
-  width: 100%;
-  min-height: 640px;
+  width: 65%;
+  height: 100%;
+  max-height: 700px;
+  min-height: 700px;
   margin-top: 24px;
-  border-radius: 8px;
+  border-radius: 12px;
   background: #fff;
+  overflow: hidden;
+`;
+
+export const ProjectImage = styled.div`
+  width: 100%;
+  height: 50%;
+  border-top-right-radius: 12px;
+  border-top-left-radius: 12px;
+  background: #33333390;
+  display: flex;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`;
+
+export const ProjectInfo = styled.div`
+  padding: 32px;
+  color: #333;
+
+  .project_title {
+    font-size: 36px;
+    font-weight: 800;
+    color: #333;
+  }
+
+  .project_type {
+    margin-top: 4px;
+    font-size: 300;
+    color: #232fa9;
+  }
+
+  .project_explain {
+    margin-top: 16px;
+    font-size: 22px;
+  }
+
+  .project_role {
+    margin-top: 10px;
+    font-size: 18px;
+
+    li {
+      margin-top: 8px;
+      margin-left: 32px;
+      list-style: disc;
+    }
+  }
 `;
